b730ba763763e50215803b733bf086ab473c2ce0
Lixianqi666 <3423554221@qq.com>
2026-05-25 18:48:50 +0800

    fix: CORS 添加 PATCH 方法

--- backend/app/main.py
+++ backend/app/main.py
@@ -60,7 +60,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins_list,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
     allow_headers=["Authorization", "Content-Type"],
 )
 
